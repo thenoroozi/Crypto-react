25c02daea35dad8840e1b609645e32c95533ae12
Hamed Noroozi <noruzi206@gmail.com>
2023-09-25 19:16:14 +0330

    add filter for search

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -9,32 +9,38 @@ import Coin from './coin';
 const Landing = () => {
 
    const [coins, setCoins] = useState([])
+   const [search, setSearch] = useState("")
 
    useEffect(() => {
       const fetchAPI = async () => {
          const data = await getCoin();
-         console.log(data);
          setCoins(data)
       }
 
       fetchAPI()
    }, [])
 
+   const searchHandler = event => {
+      setSearch(event.target.value)
+   }
+
+   const searchedCoins = coins.filter(coin => coin.name.toLowerCase().includes(search.toLowerCase()))
+   
    return (
       <div>
-         <input type="text" placeholder='Search...' />
+         <input type="text" placeholder='Search...' value={search} onChange={searchHandler} />
          {
             coins.length ?
                <div>
                   {
-                     coins.map(coin => <Coin 
-                          key={coin.id}
-                          name={coin.name}
-                          image={coin.image}
-                          symbol={coin.symbol}
-                          price={coin.current_price}
-                          marketCap={coin.market_cap}
-                          priceChange={coin.price_change_percentage_24h}
+                     searchedCoins.map(coin => <Coin
+                        key={coin.id}
+                        name={coin.name}
+                        image={coin.image}
+                        symbol={coin.symbol}
+                        price={coin.current_price}
+                        marketCap={coin.market_cap}
+                        priceChange={coin.price_change_percentage_24h}
                      />)
                   }
                </div> :
